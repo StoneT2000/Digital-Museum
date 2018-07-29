a9c932b84422a63ddc8af7c0de5cdda3cfbe0e3a
Storms2000 <stoneztao@gmail.com>
2018-07-29 12:02:59 -0400

    Added full screen mode

--- scripts/museum.js
+++ scripts/museum.js
@@ -80,7 +80,9 @@ $(document).on("ready",function(){
       $(".floor").css("transition", "transform 2s");
     }
   }
-  
+  $(".full_screen").on("click", function(){
+    toggleFullScreen();
+  })
   
 });
 
@@ -126,4 +128,29 @@ function change_floor(new_floor){
     }
   }
   
+}
+
+function toggleFullScreen() {
+  if (!document.fullscreenElement &&    // alternative standard method
+      !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+      document.documentElement.msRequestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
 }
